7415e274f74a54606fd2da21c8007faf9068c7ef
Baimorn <baimoen55@gmail.com>
2023-03-08 23:12:39 +0700

    fix: bug

--- src/data/socialMediaIcons.jsx
+++ src/data/socialMediaIcons.jsx
@@ -1,23 +1,29 @@
+import fbIcon from "../../public/image/socialMediaIcons/fb-icon.png"
+import twIcon from "../../public/image/socialMediaIcons/tw-icon.png"
+import igIcon from "../../public/image/socialMediaIcons/ig-icon.png"
+import emailIcon from "../../public/image/socialMediaIcons/email-icon.png"
+
+
 const socialMediaIcons = [
 
 {   name: "facebook",
-    icon: "../../public/image/socialMediaIcons/fb-icon.png",
+    icon: fbIcon,
     bgColor:"#728CF7"
 
 },
 {   name: "twitter",
-    icon: "../../public/image/socialMediaIcons/tw-icon.png",
+    icon: twIcon,
     bgColor:"#E17094" 
 
 },
 {   name: "instagram",
-    icon: "../../public/image/socialMediaIcons/ig-icon.png",
+    icon: igIcon,
     bgColor:"#68AAF3" 
 
 },
 
 {   name: "email",
-    icon: "../../public/image/socialMediaIcons/email-icon.png",
+    icon: emailIcon,
     bgColor:"#FF9B70"
   
 },
--- src/data/softSkillDetail.jsx
+++ src/data/softSkillDetail.jsx
@@ -1,27 +1,33 @@
+import icon1 from "../../public/image/soft-skill-icon/icon-1.png"
+import icon2 from "../../public/image/soft-skill-icon/icon-2.png"
+import icon3 from "../../public/image/soft-skill-icon/icon-3.png"
+import icon4 from "../../public/image/soft-skill-icon/icon-4.png"
+
+
 const softSkillDetail =[
 
 { 
     skill: "Communicative",
     detail: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. ",
-    icon: "../../public/image/soft-skill-icon/icon-1.png"
+    icon: icon1
 },
 
 { 
     skill: "Professional",
     detail: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. ",
-    icon: "../../public/image/soft-skill-icon/icon-2.png"
+    icon: icon2
 },
 
 { 
     skill: "Collaborative",
     detail: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. ",
-    icon: "../../public/image/soft-skill-icon/icon-3.png"
+    icon: icon3
 },
 
 { 
     skill: "Client's Favourite",
     detail: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. ",
-    icon: "../../public/image/soft-skill-icon/icon-4.png"
+    icon: icon4
 },
 
 ]
